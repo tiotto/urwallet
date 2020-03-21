532dac1807af7e984b1f765ce577e39bcb2a6b57
Thays Giotto <giotto.thays@gmail.com>
2020-03-21 13:10:19 -0300

    fix: missing sample tokens

---
+++ src/theme/tokens.js
@@ -0,0 +1,82 @@
+import { css } from 'styled-components'
+
+const Tokens = css`
+
+  :root {
+    --base-primary-dark: #ececf5;
+    --base-primary-light: #f6f6fa;
+    --base-primary: #fdfdfd;
+    --base-secondary-dark: #000;
+    --base-secondary-light: #192742;
+    --base-secondary: #dadae4;
+    --base-tertiary-light: #dae5df;
+    --base-tertiary: #ede6f2;
+    --bg-primary: #f4f4f4;
+    --bg-secondary: #fff;
+    --bg-session: #efefef;
+    --bg-tertiary: #1A202B;
+    --border-primary: #f4f4f4;
+    --box-shadow-flat: none;
+    --box-shadow-hover: 0 4px 8px rgba(0, 0, 0, .2);
+    --box-shadow-modal: 0 14px 20px rgba(0, 0, 0, .1);
+    --box-shadow-raised: 0 2px 4px rgba(0, 0, 0, .15);
+    --box-shadow-spotlight: 0 6px 12px 0 rgba(0, 0, 0, .1);
+    --elevation-flat: none;
+    --elevation-hover: 1100;
+    --elevation-modal: 1300;
+    --elevation-raised: 1000;
+    --elevation-spotlight: 1200;
+    --feedback-error: #d40d12;
+    --feedback-success: #068320;
+    --feedback-warning: #dc9a19;
+    --font-size-body1: 16px;
+    --font-size-body2: 14px;
+    --font-size-body3: 16px;
+    --font-size-body4: 14px;
+    --font-size-caption1: 12px;
+    --font-size-caption2: 10px;
+    --font-size-display1: 90px;
+    --font-size-display2: 64px;
+    --font-size-link1: 16px;
+    --font-size-link2: 14px;
+    --font-size-link3: 16px;
+    --font-size-link4: 14px;
+    --font-size-overline: 10px;
+    --font-size-subtitle1: 18px;
+    --font-size-subtitle2: 16px;
+    --font-size-title1: 48px;
+    --font-size-title2: 36px;
+    --font-size-title3: 28px;
+    --font-size-title4: 22px;
+    --font-size-title5: 20px;
+    --font-weight-bold: 700;
+    --font-weight-regular: 400;
+    --font-weight-semibold: 600;
+    --line-height-body1: 24px;
+    --line-height-body2: 17px;
+    --line-height-link1: 24px;
+    --line-height-overline: 15px;
+    --line-height-title1: 72px;
+    --line-height-title2: 54px;
+    --shape-circle: 50%;
+    --shape-line: 0;
+    --shape-oval: 40px;
+    --shape-rounded: 4px;
+    --shape-straight: 0;
+    --size-lg: 1280px;
+    --size-md: 600px;
+    --size-sm: 360px;
+    --size-xl: 1332px;
+    --size-xs: 0;
+    --text-breadcrumb: #337ab7;
+    --text-primary-dark: #192742;
+    --text-primary-footer: #777;
+    --text-primary-light: #777;
+    --text-primary: #414141;
+    --text-secondary-dark: #ececf5;
+    --text-secondary-light: #f6f6fa;
+    --text-secondary: #fff;
+  }
+`
+
+export default Tokens
