048fd722772767c7e5697039db1f3d4291fc3b3a
Thays Giotto <giotto.thays@gmail.com>
2020-03-21 13:00:20 -0300

    feat: add login page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './pages/Home';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Login from './pages/Login'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<Login />, document.getElementById('root'))
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
---
+++ src/pages/Login/Login.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Login = () =>
+  <>
+  </>
+
+export default Login
