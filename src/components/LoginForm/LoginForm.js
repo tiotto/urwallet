a0ec7ff46ca175cc83e8a24092747b44c0fdb8cc
Thays Giotto <giotto.thays@gmail.com>
2020-04-01 20:30:12 -0300

    feat: redesign login page

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,50 +1,84 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import { withRouter } from 'react-router-dom'
 
+import { useGlobalState } from '../../context'
+import { login } from '../../services/urwallet/auth'
 import Input from '../Atoms/Input'
-import { ReactComponent as Logo } from '../../assets/logo.svg'
 import Button from '../../components/Atoms/Button'
+import api from '../../services/urwallet/api'
 
-const LoginForm = () =>
-  <S.Login>
-    <SmallerLogo />
-    <Input
-      className='login'
-      type='email'
-      placeholder='Insira seu email'
-    />
-    <Input
-      className='password'
-      type='password'
-      placeholder='Insira sua senha'
-    />
-    <Button text='Entrar' />
-    <Button text='Inscrever' />
-  </S.Login>
+const LoginForm = ({ history }) => {
+  const globalState = useGlobalState()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
-const SmallerLogo = styled(Logo)`
-  height: 30px;
-  margin-bottom: 28px;
-`
+  const handleSubmit = async e => {
+    e.preventDefault()
+
+    try {
+      const response = await api.post('/sessions', { email, password })
+      login(response.data.token)
+
+      const id = response.data.account.id
+
+      global.localStorage.setItem('urwallet_userId', id)
+      global.localStorage.setItem('urwallet_userEmail', email)
+
+      globalState.setUser({ id, email })
+
+      history.push('/dashboard')
+    } catch (err) {
+      console.log(err, 'Tente novamente.')
+    }
+  }
+
+  return (
+    <S.Login onSubmit={handleSubmit}>
+      <S.Fields>
+        <Input
+          className='login'
+          type='email'
+          placeholder='Insira seu email'
+          onChange={e => setEmail(e.target.value)}
+          required
+        />
+        <Input
+          className='password'
+          type='password'
+          placeholder='Insira sua senha'
+          onChange={e => setPassword(e.target.value)}
+          required
+        />
+      </S.Fields>
+      <Button text='Entrar' />
+      <S.Signup>Novo por aqui? <a href="#">Crie uma nova conta</a></S.Signup>
+    </S.Login>
+  )
+}
 
 const S = {
   Login: styled.form`
-    box-shadow: var(--box-shadow-raised);
-    background: #FFF;
-    width: 380px;
-    padding: 42px 20px;
     text-align: center;
-    margin: auto;
-    position: absolute;
-    top: 50%;
-    right: 0;
-    left: 0;
-    transform: translateY(-50%);
-
-    .input {
+  `,
+  Fields: styled.div`
+    .field {
       margin-bottom: 12px;
+
+      &:last-child{
+        margin-bottom: 28px;
+      }
+  }
+  `,
+  Signup: styled.div`
+    margin-top: 22px;
+    text-align: center;
+    display: block;
+
+    a {
+      color: #000;
     }
   `
 }
 
-export default LoginForm
+export default withRouter(LoginForm)
