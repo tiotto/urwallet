a78ef8f50c2a74b5f8e2e43f21ec9ad077a3a19f
Thays Giotto <giotto.thays@gmail.com>
2020-04-04 14:42:40 -0300

    feat: update menu with new pages

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const Menu = () =>
   <S.Menu>
     <S.MenuItems>
-      <S.MenuItem>Comprar/Vender</S.MenuItem>
-      <S.MenuItem>Trocar</S.MenuItem>
+      <S.MenuItem>
+        <Link to='/exchange'>
+          Comprar/Vender
+        </Link>
+      </S.MenuItem>
+      <S.MenuItem>
+        <Link to='/trade'>
+          Trocar
+        </Link>
+      </S.MenuItem>
     </S.MenuItems>
   </S.Menu>
 
