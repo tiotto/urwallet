3aceab2f531ff9d2aa395d223ad7689cf36e4564
Thays Giotto <giotto.thays@gmail.com>
2020-04-05 10:10:33 -0300

    feat: redesign balance

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -7,8 +7,10 @@ import { formatCurrency } from '../../utils/currencyFormatter'
 const Balance = ({ currency, balance, amount }) =>
   <S.Balance>
     <S.Currency>{currency}</S.Currency>
-    <S.Amount>{amount}</S.Amount>
-    <S.Value>{formatCurrency(balance, 'BRL')}</S.Value>
+    <S.Content>
+      {amount && <S.Amount>{amount}</S.Amount>}
+      <S.Value>{formatCurrency(balance, 'BRL')}</S.Value>
+    </S.Content>
   </S.Balance>
 
 Balance.propTypes = {
@@ -19,29 +21,33 @@ Balance.propTypes = {
 
 const S = {
   Balance: styled.section`
-    box-shadow: 0 1px 4px rgba(0,0,0,.04);
-    width: 180px;
+    border: 1px solid #E5EEF6;
+    width: 215px;
     height: 120px;
-    padding: 20px;
+    /* padding: 20px; */
     text-align: center;
-    justify-content: center;
-    display: flex;
     flex-direction: column;
     background: #FFF;
     border-radius: 4px;
     margin: 26px 18px 44px 0;
   `,
   Currency: styled.div`
+    color: #707AAD;
+    background:#F3F5FF;
     font-size: 16px;
     font-weight: 600;
     margin-bottom: 8px;
+    padding: 12px 18px 8px;
+    text-align: left;
+  `,
+  Content: styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 12px 18px;
   `,
   Amount: styled.div`
-    font-size: 18px;
-    margin-bottom: 8px;
   `,
   Value: styled.div`
-    font-size: 12px;
   `
 }
 
--- src/components/Balances/Balances.js
+++ src/components/Balances/Balances.js
@@ -6,7 +6,7 @@ import Balance from '../Balance'
 
 const Balances = () =>
   <S.Balances>
-    <Balance currency='Britas' balance='100000' amount='2' />
+    <Balance currency='Brita' balance='100000' amount='2' />
     <Balance currency='Bitcoin' balance='1231800' amount='8' />
     <Balance currency='Reais' balance='131928' />
   </S.Balances>
