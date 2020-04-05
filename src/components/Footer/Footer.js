40af3a7874dbc8318d93199d6fb0de1cf9b47fd6
Thays Giotto <giotto.thays@gmail.com>
2020-04-05 10:11:19 -0300

    feat: redesign footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { ReactComponent as Logo } from '../../assets/logo.svg'
+
 const Footer = () =>
   <S.Footer>
-    <S.Text>made with <span role='img' aria-label='coffee'>☕️</span> by @thaysthays</S.Text>
+    <S.Text><StyledLogo />made with <span role='img' aria-label='coffee'>☕️</span> by @thaysthays</S.Text>
   </S.Footer>
+
+const StyledLogo = styled(Logo)`
+  height: 14px;
+  margin-right: 6px;
+`
 
 const S = {
   Footer: styled.footer`
-    background: #363a3c;
+    background: #403F4B;
     color: #FFF;
     padding: 16px;
 
@@ -18,7 +25,7 @@ const S = {
   `,
   Text: styled.p`
     text-align: center;
-    color: #c4c4c4;
+    color: #fff;
   `
 }
 
